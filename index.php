3ec61a57bb3b90f5a40049126b08849876208c94
Jason Semko <jsemko@hugeinc.com>
2012-03-19 14:31:03 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">	
+			
+		<title>Sigma Aldrich Prototype (3PP)</title>
+		
+		<!-- CSS -->
+		<link href="css/base.css" rel="stylesheet">	
+		<link href="css/global.css" rel="stylesheet">	
+		
+		<!-- Load modernizr first - all other scripts at bottom of page -->
+		<script src="js/libs/modernizr-2.0.6.min.js"></script>
+	</head> 
+
+	<body class="homePage">
+		<!--[if lt IE 7 ]> <div class="ie ie6"> <![endif]-->
+		<!--[if IE 7 ]> <div class="ie ie7"> <![endif]-->
+		<!--[if IE 8 ]> <div class="ie ie8"> <![endif]-->
+		<!--[if IE 9 ]> <div class="ie ie9"> <![endif]-->
+
+		<!-- HUGE Scripts -->
+		<script defer src="js/plugins.js"></script>
+		<script defer src="js/global.js"></script>		
+	</body>
+</html>
